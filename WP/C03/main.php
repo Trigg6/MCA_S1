9d37dfaf7f1c0122efbff82c157061adbab5fbe4
Trigg6 <mhdshahal411@gmail.com>
2026-01-03 15:24:51 +0530

    Co5

---
+++ WP/C03/main.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <?php
+            $a=10;
+            $b=20;
+            $temp;
+            print("Before Swapping a=$a b=$b;");
+            $temp=$a;
+            $a=$b;
+            $b=$temp;
+            print("<br>After Swapping a=$a b=$b;");
+            
+        ?>
+    </body>
